034d84e4bca385097780f489374067adc7aeb0ff
Emmanuel Frecon <emmanuel.frecon@myjoice.com>
2017-09-25 22:28:54 +0200

    Don't log to syslog and stderr to avoid double rows in docker log output

--- pptpd/iptables-update.sh
+++ pptpd/iptables-update.sh
@@ -17,7 +17,7 @@ for port in "$@"; do
             # If we have a host and it is not an IP number, then we can start comparing
             isip=$(echo "${host}" | grep -E '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$')
             if [ -z "$isip" ]; then
-                logger -s -p user.debug "Checking if ${host} is still at ${in_tables}"
+                logger -p user.debug "Checking if ${host} is still at ${in_tables}"
                 current=$(dig +short $host | tail -n 1)
                 # We don't do anything when the host seem to have disappear,
                 # maybe should we actually remove the rule.
@@ -25,7 +25,7 @@ for port in "$@"; do
                     # Host has changed IP address, update the ip tables.
                     if [ "$current" != "$in_tables" ]; then
                         rulenum=$(echo "${line}" | cut -d" " -f1)
-                        logger -s -p user.info "${host} has moved from ${in_tables} to $current, updating rule #$rulenum"
+                        logger -p user.info "${host} has moved from ${in_tables} to $current, updating rule #$rulenum"
                         iptables -R INPUT $rulenum -i eth0 -s $current -p tcp --dport $port -j ACCEPT -m comment --comment "${host}"
                     fi
                 fi
