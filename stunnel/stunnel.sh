89000991010cec6c1dc705cb21e37d0ee524ab99
Emmanuel Frecon <emmanuel.frecon@myjoice.com>
2018-05-17 14:17:15 +0200

    Default is an empty internal log file, stunnel will only log on stdout/err which will be captured by Docker. This puts disk space in control

--- stunnel/stunnel.sh
+++ stunnel/stunnel.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# Initialise all STUNNEL_ environment variables to good defaults. STUNNEL_LOG
+# can also be given, in which case it should point to the path of a log file.
+# The default is to keep it empty to rely on Docker logging and keep the
+# container volume to grow on disk.
 export STUNNEL_CONF="/etc/stunnel/stunnel.conf"
 export STUNNEL_DEBUG="${STUNNEL_DEBUG:-7}"
 export STUNNEL_CLIENT="${STUNNEL_CLIENT:-no}"
@@ -7,6 +11,7 @@ export STUNNEL_CAFILE="${STUNNEL_CAFILE:-/etc/ssl/certs/ca-certificates.crt}"
 export STUNNEL_KEY="${STUNNEL_KEY:-/etc/stunnel/stunnel.key}"
 export STUNNEL_CRT="${STUNNEL_CRT:-/etc/stunnel/stunnel.pem}"
 export STUNNEL_PSK="${STUNNEL_PSK:-}"
+#export STUNNEL_LOG
 
 if [[ -z "${STUNNEL_SERVICE}" ]] || [[ -z "${STUNNEL_ACCEPT}" ]] || [[ -z "${STUNNEL_CONNECT}" ]]; then
     echo >&2 "one or more STUNNEL_SERVICE* values missing: "
@@ -16,9 +21,15 @@ if [[ -z "${STUNNEL_SERVICE}" ]] || [[ -z "${STUNNEL_ACCEPT}" ]] || [[ -z "${STU
     exit 1
 fi
 
+env
+
 if [[ ! -z "${STUNNEL_PSK}" ]] && [[ -f ${STUNNEL_PSK} ]]; then
     if [[ ! -s ${STUNNEL_CONF} ]]; then
-        cat /srv/stunnel/stunnel-psk.conf.template | envsubst > ${STUNNEL_CONF}
+        if [[ -z "${STUNNEL_LOG}" ]]; then
+            cat /srv/stunnel/stunnel-psk.conf.template | envsubst | sed "s/^output\\s*=.*/;&/g" > ${STUNNEL_CONF}
+        else
+            cat /srv/stunnel/stunnel-psk.conf.template | envsubst > ${STUNNEL_CONF}
+        fi
     fi
 else
     if [[ ! -f ${STUNNEL_KEY} ]]; then
@@ -36,7 +47,11 @@ else
     update-ca-certificates
 
     if [[ ! -s ${STUNNEL_CONF} ]]; then
-        cat /srv/stunnel/stunnel.conf.template | envsubst > ${STUNNEL_CONF}
+        if [[ -z "${STUNNEL_LOG}" ]]; then
+            cat /srv/stunnel/stunnel.conf.template | envsubst | sed "s/^output\\s*=.*/;&/g" > ${STUNNEL_CONF}
+        else
+            cat /srv/stunnel/stunnel.conf.template | envsubst > ${STUNNEL_CONF}
+        fi
     fi
 fi
 
