3bbea74263fdfe3fc68d1725ff99af17f97379ee
Emmanuel Frécon <efrecon@gmail.com>
2018-08-12 21:40:13 +0200

    Adding Disque

---
+++ disque/README.md
@@ -0,0 +1,49 @@
+# Disque Docker Image
+
+This is a production ready Docker minimal image for running [Disque], an
+in-memory, distributed job queue from the same [author][antirez] as [redis].
+The image is based on [Alpine] and configuration can happen through setting environment variables starting with `DISQUE_`.
+
+## Usage
+
+To create a disque server, run the following command:
+
+    docker run efrecon/disque:1.0-rc1
+
+Any configuration variable that appears in the default Disque [configuration] can be set in two different ways:
+
+1. By appending a command-line option which name is the same as the
+   configuration variable, though led by a double-dash. This is as per the
+   Disque main server behaviour.
+
+2. By setting an environment variable according to the following rules: the
+   environment variable will be in uppercase, all single dashes from the Disque
+   configuration variable should be replaced by underscores and the environment
+   variable should start with `DISQUE_`.
+
+So, to set the port, you could run any of the two commands below:
+
+    docker run efrecon/disque:1.0-rc1 --port 3456
+    docker run --env DISQUE_PORT=3456 efrecon/disque:1.0-rc1
+
+## Implementation
+
+This image takes its roots in [richnorth/disque], while being based on the
+minimal [Alpine] image and taking heavy inspiration of the [official] redis
+image. The image is directly compiled from the official released source. To
+ensure proper compilation on Alpine, support for backtrace is manually removed
+from [config.h] in the [Dockerfile](./Dockerfile).
+
+Support for environment variables led by `DISQUE_` is implemented as part of the
+entrypoint. The implementation will copy the default configuration file from
+Disque, modify it per the environment variables and start with the disque server
+using the modified copy of the default configuration file.
+
+  [Disque]: https://github.com/antirez/disque
+  [antirez]: https://github.com/antirez/
+  [redis]: https://github.com/antirez/redis
+  [richnorth/disque]: https://hub.docker.com/r/richnorth/disque/
+  [Alpine]: https://hub.docker.com/_/alpine/
+  [official]: https://github.com/docker-library/redis/tree/master/4.0/alpine
+  [config.h]: https://github.com/antirez/disque/blob/master/src/config.h
+  [configuration]: https://github.com/antirez/disque/blob/master/disque.conf
---
+++ disque/docker-entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+# or first arg is `something.conf`
+if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
+	set -- disque-server "$@"
+fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'disque-server' -a "$(id -u)" = '0' ]; then
+	chown -R disque .
+
+    # First arg is not a configuration file and we have environment variables
+    # starting with DISQUE_. Arrange to use a configuration file and to replace
+    # configuration variables in that file using the values from the environment
+    # variables (sans the leading DISQUE_ and in lower case, all underscores
+    # become dash).
+    DENV=$(env | grep -E '^DISQUE_' | wc -l)
+    if [ "${2%.conf}" = "$2" ] && [ "$DENV" -gt 0 ]; then
+        cp /usr/local/share/disque.conf ./disque.conf
+        for VAR in $(env); do
+            if [ -n "$(echo $VAR | grep -E '^DISQUE_')" ]; then
+                # Transform environment variable (starting with DISQUE_) into a
+                # variable that might occur in the configuration file. This
+                # remove the leading DISQUE_, change to lowercase and replace
+                # all underscores by single dash.
+                VAR_NAME=$(echo "$VAR" | sed -r "s/DISQUE_([^=]*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr '_' '-' )
+                # Keep a record of the name of the real environment variable,
+                # including the leading DISQUE_
+                VAR_FULL_NAME=$(echo "$VAR" | sed -r "s/([^=]*)=.*/\1/g")
+                if [ -n "$(cat ./disque.conf |grep -E "^(^|^#*)$VAR_NAME")" ]; then
+                    echo "Configuring '$VAR_NAME' from env: $(eval echo \$$VAR_FULL_NAME)"
+                    sed -ri "s/(^#*)($VAR_NAME)\s+(.*)/\2 $(eval echo \$$VAR_FULL_NAME|sed -e 's/\//\\\//g')/g" ./disque.conf
+                    sed -ri "s/(^#*)($VAR_NAME)\s*$/$VAR_NAME $(eval echo \$$VAR_FULL_NAME|sed -e 's/\//\\\//g')/g" ./disque.conf
+                    # Remove the variable, this provides *some* sort of privacy
+                    # through keeping away possible secrets to leak at runtime
+                    # (they will still be in the configuration file though...
+                    # but this requires access to the container's disk
+                    # hierarchy).
+                    unset $VAR_FULL_NAME
+                fi
+            fi
+        done
+        chown -R disque ./disque.conf
+        # Shift away disque-server to be able to prepend current configuration
+        # file to list of (otherwise arguments)
+        shift
+        exec su-exec disque disque-server ./disque.conf "$@"
+    else
+        exec su-exec disque "$0" "$@"
+    fi
+fi
+
+exec "$@"
