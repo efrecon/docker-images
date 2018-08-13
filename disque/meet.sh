3d6a3e94affd7c3270a02f75cc0e3c2eff3054b8
Emmanuel Frécon <efrecon@gmail.com>
2018-08-13 11:10:34 +0200

    Checking for presence of remote Disque server

--- disque/meet.sh
+++ disque/meet.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+
 set -e
 
 HOST=localhost
 PORT=
 NODES=./nodes.conf
 WAIT=2
+MAX=-1
 
-while getopts "h:p:n:w:" opt; do
+while getopts "h:p:f:w:c:" opt; do
     case $opt in
         h)
             HOST="$OPTARG"
@@ -14,11 +16,14 @@ while getopts "h:p:n:w:" opt; do
         p)
             PORT="$OPTARG"
             ;;
-        n)
+        f)
             NODES="$OPTARG"
             ;;
         w)
             WAIT="$OPTARG"
+            ;;
+        c)
+            MAX="$OPTARG"
             ;;
         \?)
             set +x
@@ -35,10 +40,13 @@ done
 shift $(expr $OPTIND - 1)
 
 # Wait for Disque to be properly started through discovering the existence of
-# the nodes configuration file.
-while  [ ! -f "$NODES" ]; do
+# the nodes configuration file. (see: https://stackoverflow.com/a/6270803 for
+# test)
+COUNT=$MAX
+while { [ -n "$NODES" ] && [ ! -f "$NODES" ]; } && { [ $MAX -lt 0 ] || [ $COUNT -gt 0 ]; }; do
     echo "Waiting for Disque to start..."
     sleep $WAIT
+    COUNT=$(($COUNT - 1))
 done
 
 # Discover port of locally running disque-server whenever possible and
@@ -52,7 +60,7 @@ fi
 
 # Defaulting to port 7711 for the disque-server whenever none is available.
 if [ -z "$PORT" ]; then
-    echo "Defaulting to port 7711"
+    echo "Defaulting to port 7711 for local Disque server connection"
     PORT=7711
 fi
 
@@ -64,7 +72,30 @@ for REMOTE in "$@"; do
     if [ "$RPORT" = "" ]; then
         RPORT=7711
     fi
-    echo "Joining Disque remote at ${RHOST}:${RPORT}"
-    disque -p $PORT -h $HOST cluster meet "$RHOST" "$RPORT"
-done
 
+    # Stop stopping on errors, since we use error reporting to detect remote host
+    # presence. 
+    set +e
+
+    COUNT=$MAX
+    while true; do
+        # Test availability of remote host and port using nc in reporting mode
+        # and stop as soon as the port is available or we have reached the max
+        # number of attempts.
+        echo "Checking availability of ${RHOST}:${RPORT}"
+        nc -z $RHOST $RPORT
+        if [ ! "$?" ] || { [ $MAX -gt 0 ] && [ $COUNT -le 1 ]; }; then
+            break
+        fi
+        COUNT=$(($COUNT - 1))
+        sleep $WAIT
+    done
+
+    set -e
+    if [ ! "$?" ]; then
+        echo "Joining Disque remote at ${RHOST}:${RPORT}"
+        disque -p $PORT -h $HOST cluster meet "$RHOST" "$RPORT"
+    else
+        echo "!! No Disque server at ${RHOST}:${RPORT}, cannot join"
+    fi
+done
