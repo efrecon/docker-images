7e368574100e2668cef39b170118e7c11d29368a
Emmanuel Frécon <efrecon@gmail.com>
2018-11-14 22:07:48 +0100

    Reading password from file whenever instructed to

--- healthcheck/postgres/healthcheck.sh
+++ healthcheck/postgres/healthcheck.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-set -eo pipefail
+set -e
 
 host="$(hostname -i || echo '127.0.0.1')"
 user="${POSTGRES_USER:-postgres}"
 db="${POSTGRES_DB:-$POSTGRES_USER}"
+# Capture password from file if _FILE env. variable is set.
+if [ -n "${POSTGRES_PASSWORD_FILE}" ]; then
+	export POSTGRES_PASSWORD=$(cat "${POSTGRES_PASSWORD_FILE}")
+fi
 export PGPASSWORD="${POSTGRES_PASSWORD:-}"
 
 if select="$(echo 'SELECT 1' | psql --host "$host" --username "$user" --dbname "$db" --quiet --no-align --tuples-only)" && [ "$select" = '1' ]; then
-	echo "ALive"
+	echo "Alive"
 	exit 0
 fi
 
