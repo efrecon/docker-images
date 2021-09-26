7ed103d7fa84d956a4ab5559ef7b6f265366898d
Emmanuel Frécon <efrecon@gmail.com>
2021-09-26 23:02:18 +0200

    add auto install

--- binenv/distribution/binenv.sh
+++ binenv/distribution/binenv.sh
@@ -5,6 +5,7 @@ ROOT_DIR=${ROOT_DIR:-"$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd
 set -eu
 
 # This script is a binenv wrapper that can be used to:
+# 0. install binenv itself if it is not already present
 # 1. install a binenv distribution
 # 2. Get the list of the known binenv distributions
 # 3. Get the known versions of a binenv distribution
@@ -33,16 +34,18 @@ BINENV_AGE=${BINENV_AGE:-300}
 # $PATH.
 BINENV_BIN=${BINENV_BIN:-binenv}
 
+# Project and location at GitHub. You shouldn't have to change that really...
+BINENV_GHPROJ=${BINENV_GHPROJ:-devops-works/binenv}
+BINENV_ROOTURL=${BINENV_ROOTURL:-https://github.com/${BINENV_GHPROJ}}
+
 # Set this to 1 for more verbosity.
 BINENV_VERBOSE=${BINENV_VERBOSE:-0}
-
-BINENV_THIS=$0
 
 usage() {
   # This uses the comments behind the options to show the help. Not extremly
   # correct, but effective and simple.
   echo "$0 is a binenv wrapper with following options:" && \
-    grep "[[:space:]].)\ #" "$BINENV_THIS" |
+    grep "[[:space:]].)\ #" "$0" |
     sed 's/#//' |
     sed -r 's/([a-z])\)/-\1/'
   exit "${1:-0}"
@@ -68,12 +71,25 @@ shift $((OPTIND-1))
 
 
 _verbose() {
-  [ "$BINENV_VERBOSE" = "1" ] && printf %s\\n "$1" >&2
+  if [ "$BINENV_VERBOSE" = "1" ]; then
+    printf %s\\n "$1" >&2
+  fi
 }
 
 _error() {
   printf %s\\n "$1" >&2
   exit 1
+}
+
+_download() {
+  _verbose "Downloading $1"
+  if command -v curl >&2 >/dev/null; then
+    curl -sSL "$1"
+  elif command -v wget >&2 >/dev/null; then
+    wget -q -O - "$1"
+  else
+    _error "Can neither find curl, nor wget for downloading"
+  fi
 }
 
 _distributions() {
@@ -102,14 +118,35 @@ _versions() {
     grep -E '^[0-9]+\.[0-9]+\.[0-9]+$'
 }
 
-# Provided a proper binenv binary, create a binenv mini-environment under its
-# regular directory for binary installation when that directory does not exist.
+# Create the binenv hidden directory if it does not exist yet.
 if ! [ -d "$BINENV_BINDIR" ]; then
-  if command -v "$BINENV_BIN" >/dev/null 2>&1; then
-    _verbose "Creating missing binenv environment under $BINENV_BINDIR"
-    mkdir -p "$BINENV_BINDIR"
-    ln -s "$(command -v "$BINENV_BIN")" "${BINENV_BINDIR%/}/shim"
+  _verbose "Creating missing binenv directory $BINENV_BINDIR"
+  mkdir -p "$BINENV_BINDIR"
+fi
+
+# Automatically install the binenv directory in a way that is much similar to
+# the way binenv installs itself into the $BINENV_BINDIR directory.
+if ! command -v "$BINENV_BIN" >/dev/null 2>&1; then
+  if [ -f "${BINENV_BINDIR}/${BINENV_BIN}" ] && [ -x "${BINENV_BINDIR}/${BINENV_BIN}" ]; then
+    # Just recapture the binenv from the BINENV_BIN directory into the PATH when
+    # we have found an executable with the right name.
+    export PATH=${BINENV_BINDIR}:$PATH
+  else
+    # Discover the latest version from the HTML page (maybe should we use the
+    # API, but this avoids rate-limiting).
+    version=$(_download "${BINENV_ROOTURL}/releases"|grep "href=\"/${BINENV_GHPROJ}/releases/tag/v[0-9].[0-9]*.[0-9]*\"" | grep -v no-underline | head -n 1 | cut -d '"' -f 2 | awk '{n=split($NF,a,"/");print a[n]}' | awk 'a !~ $0{print}; {a=$0}')
+    _verbose "Installing binenv v. ${version#v*} into $BINENV_BINDIR"
+    # Download and install binenv, and arrange for the shim to be created.
+    _download "${BINENV_ROOTURL}/releases/download/v${version#v*}/binenv_linux_amd64" > "${BINENV_BINDIR}/${BINENV_BIN}"
+    chmod a+x "${BINENV_BINDIR}/${BINENV_BIN}"
+    export PATH=${BINENV_BINDIR}:$PATH
   fi
+fi
+
+# Create the shim if it does not exist.
+if ! [ -x "${BINENV_BINDIR%/}/shim" ]; then
+  _verbose "Creating missing shim under $BINENV_BINDIR"
+  ln -s "$(command -v "$BINENV_BIN")" "${BINENV_BINDIR%/}/shim"
 fi
 
 # Update distribution cache when current file is too old or does not exit.
